a0aaac3069c8cb770de5dc92ed267dd782f28486
Renildo Santos <renildosantos626@gmail.com>
2022-08-08 19:03:02 -0300

    primeiro commit

---
+++ src/main/java/DIO/Projetobasespring/ProjetoBaseSpringApplication.java
@@ -0,0 +1,13 @@
+package DIO.Projetobasespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBaseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBaseSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DIO/Projetobasespring/ProjetoBaseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package DIO.Projetobasespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBaseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
